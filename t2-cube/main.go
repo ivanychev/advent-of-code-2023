31a9f6ced5a3fa8582bda6c3a0782d6cf6a386ba
Sergey Ivanychev <sergeyivanychev@gmail.com>
2023-12-02 14:47:59 +0100

    Part 2

--- t2-cube/main.go
+++ t2-cube/main.go
@@ -25,10 +25,28 @@ type Inventory struct {
 	Green int
 }
 
+func (i Inventory) Power() int64 {
+	return int64(i.Red) * int64(i.Green) * int64(i.Blue)
+}
+
 func (g *Game) PossibleForInventory(i Inventory) bool {
 	return lo.EveryBy(g.Rounds, func(r GameRound) bool {
 		return r.Red <= i.Red && r.Blue <= i.Blue && r.Green <= i.Green
 	})
+}
+
+func (g *Game) MinimumInventory() Inventory {
+	return Inventory{
+		Red: lo.Max(lo.Map(g.Rounds, func(r GameRound, index int) int {
+			return r.Red
+		})),
+		Green: lo.Max(lo.Map(g.Rounds, func(r GameRound, index int) int {
+			return r.Green
+		})),
+		Blue: lo.Max(lo.Map(g.Rounds, func(r GameRound, index int) int {
+			return r.Blue
+		})),
+	}
 }
 
 func parseGameRound(rawRound string) GameRound {
@@ -77,22 +95,40 @@ func GameFromLine(line string) Game {
 	}
 }
 
+// Part 1
+
+//func main() {
+//	//rows, err := common.FileToRows("/Users/iv/Code/advent-of-code-2023/t2-cube/test.txt")
+//	rows, err := common.FileToRows("/Users/iv/Code/advent-of-code-2023/t2-cube/1.txt")
+//	inventory := Inventory{
+//		Red:   12,
+//		Green: 13,
+//		Blue:  14,
+//	}
+//	if err != nil {
+//		log.Fatalf("%w", err)
+//	}
+//	games := lo.Map(rows, common.NoIndex(GameFromLine))
+//	possibleGames := lo.Filter(games, func(item Game, index int) bool {
+//		return item.PossibleForInventory(inventory)
+//	})
+//	fmt.Printf("%d", lo.SumBy(possibleGames, func(g Game) int {
+//		return g.GameIndex
+//	}))
+//}
+
+// Part 2
 func main() {
-	//rows, err := common.FileToRows("/Users/iv/Code/advent-of-code-2023/t2-cube/test.txt")
 	rows, err := common.FileToRows("/Users/iv/Code/advent-of-code-2023/t2-cube/1.txt")
-	inventory := Inventory{
-		Red:   12,
-		Green: 13,
-		Blue:  14,
-	}
+	//rows, err := common.FileToRows("/Users/iv/Code/advent-of-code-2023/t2-cube/test.txt")
 	if err != nil {
 		log.Fatalf("%w", err)
 	}
 	games := lo.Map(rows, common.NoIndex(GameFromLine))
-	possibleGames := lo.Filter(games, func(item Game, index int) bool {
-		return item.PossibleForInventory(inventory)
+	powers := lo.Map(games, func(g Game, index int) int64 {
+		return g.MinimumInventory().Power()
 	})
-	fmt.Printf("%d", lo.SumBy(possibleGames, func(g Game) int {
-		return g.GameIndex
-	}))
+	println(lo.Reduce(powers, func(agg int64, item int64, index int) int64 {
+		return agg + item
+	}, int64(0)))
 }
