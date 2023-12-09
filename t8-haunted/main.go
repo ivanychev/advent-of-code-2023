f1794d6a8b214f68dfecf7e6ad06660b703660fa
Sergey Ivanychev <sergeyivanychev@gmail.com>
2023-12-09 13:47:54 +0100

    Part 2

--- t8-haunted/main.go
+++ t8-haunted/main.go
@@ -166,15 +166,87 @@ type CycleInfo struct {
 	firstEncounterSteps  int64
 	secondEncounterSteps int64
 	commandPointer       int64
+	endsEncounteredAt    []int64
 	edge                 *Edge
 }
+
+type CyclePosition struct {
+	info                       CycleInfo
+	length                     int64
+	coord                      int64
+	nextEndIndex               int
+	endsEncounteredInCycle     []int64
+	endsEncounteredBeforeCycle []int64
+}
+
+func (p CyclePosition) UntilNext() int64 {
+	if len(p.endsEncounteredBeforeCycle) > 0 {
+		fmt.Errorf("Not implemented")
+	}
+	if p.coord < 0 {
+		return p.endsEncounteredInCycle[p.nextEndIndex] - p.coord
+	}
+	steps := (p.endsEncounteredInCycle[p.nextEndIndex] + p.length - p.coord) % p.length
+	if steps == 0 {
+		steps = p.length
+	}
+	return steps
+}
+
+func (p *CyclePosition) StepBy(steps int64) {
+	steps = steps % p.length
+	endCoord := p.coord + steps
+	if endCoord > 0 {
+		endCoord = (p.coord + steps) % p.length
+	}
+	offset := int64(0)
+	if p.endsEncounteredInCycle[p.nextEndIndex] <= p.coord {
+		offset = p.length
+	}
+	newIndex := p.nextEndIndex
+	for offset+p.endsEncounteredInCycle[newIndex] <= steps {
+		if newIndex+1 == len(p.endsEncounteredInCycle) {
+			offset += p.length
+			newIndex = 0
+		} else {
+			newIndex++
+		}
+	}
+
+	p.coord = endCoord
+	p.nextEndIndex = newIndex
+}
+
+func prevIndex(index int64, length int64) int64 {
+	prev := index - 1
+	if prev == -1 {
+		prev = length - 1
+	}
+	return prev
+}
+
+func nextIndex(index int64, length int64) int64 {
+	next := index + 1
+	if next == length {
+		next = 0
+	}
+	return next
+}
+
+//func (p CyclePosition) AtEnd() bool {
+//	return p.endsEncounteredBeforeCycle[p.nextEndIndex] == p.coord || p.endsEncounteredBeforeCycle[prevIndex(p.nextEndIndex, p.length)] == p.coord
+//}
 
 func findCycle(e *Edge, commands []byte) CycleInfo {
 	commandPointer := int64(0)
 	totalSteps := int64(0)
+	endsEncounteredAt := make([]int64, 0)
 	edgesSet := make(map[EdgeStep]int64)
 	edgesSet[EdgeStep{e, totalSteps}] = int64(0)
 	for {
+		if e.name[LAST] == 'Z' {
+			endsEncounteredAt = append(endsEncounteredAt, totalSteps)
+		}
 		if commands[commandPointer] == 'L' {
 			e = e.left
 		} else {
@@ -188,6 +260,7 @@ func findCycle(e *Edge, commands []byte) CycleInfo {
 				firstEncounterSteps:  firstEncounterSteps,
 				secondEncounterSteps: totalSteps,
 				commandPointer:       commandPointer,
+				endsEncounteredAt:    endsEncounteredAt,
 				edge:                 e,
 			}
 		} else {
@@ -198,12 +271,61 @@ func findCycle(e *Edge, commands []byte) CycleInfo {
 
 func main() {
 	nameToEdge, commands := createEdgesAndCommands("/Users/iv/Code/advent-of-code-2023/t8-haunted/1.txt")
+	//nameToEdge, commands := createEdgesAndCommands("/Users/iv/Code/advent-of-code-2023/t8-haunted/1.txt")
 
 	edges := lo.Filter(lo.Values(nameToEdge), func(item *Edge, index int) bool {
 		return item.name[LAST] == 'A'
 	})
-	fmt.Printf("Found %d edges\n", len(edges))
-	for _, edge := range edges {
-		fmt.Printf("%+v\n", findCycle(edge, commands))
-	}
+	cycleInfos := lo.Map(edges, func(e *Edge, index int) CycleInfo {
+		return findCycle(e, commands)
+	})
+	cyclePositions := lo.Map(cycleInfos, func(c CycleInfo, index int) CyclePosition {
+		return CyclePosition{
+			info:         c,
+			coord:        -c.firstEncounterSteps,
+			length:       c.secondEncounterSteps - c.firstEncounterSteps,
+			nextEndIndex: 0,
+			endsEncounteredBeforeCycle: lo.Filter(lo.Map(c.endsEncounteredAt, func(item int64, index int) int64 {
+				return item - c.firstEncounterSteps
+			}), func(item int64, index int) bool {
+				return item < 0
+			}),
+			endsEncounteredInCycle: lo.Filter(lo.Map(c.endsEncounteredAt, func(item int64, index int) int64 {
+				return item - c.firstEncounterSteps
+			}), func(item int64, index int) bool {
+				return item >= 0
+			}),
+		}
+	})
+
+	res := lo.Reduce(cyclePositions, func(agg int64, p CyclePosition, index int) int64 {
+		var length = p.info.secondEncounterSteps - p.info.firstEncounterSteps
+		return Lcm(agg, length)
+	}, 1)
+	fmt.Printf("%d\n", res)
+
+	//for _, c := range cyclePositions {
+	//	fmt.Printf("%+v\n", c)
+	//}
+
+	//p := CyclePosition{
+	//	info: CycleInfo{
+	//		firstEncounterSteps:  0,
+	//		secondEncounterSteps: 5,
+	//		commandPointer:       0,
+	//		endsEncounteredAt:    []int64{1, 4},
+	//		edge:                 &Edge{"fdf", nil, nil},
+	//	},
+	//	length:                     5,
+	//	coord:                      0,
+	//	nextEndIndex:               0,
+	//	endsEncounteredInCycle:     []int64{1, 4},
+	//	endsEncounteredBeforeCycle: []int64{},
+	//}
+	//
+	//fmt.Printf("%+v\n", p.UntilNext())
+	//p.StepBy(1)
+	//fmt.Printf("coord: %d, %+v\n", p.coord, p.UntilNext())
+	//p.StepBy(5)
+	//fmt.Printf("coord: %d, %+v\n", p.coord, p.UntilNext())
 }
